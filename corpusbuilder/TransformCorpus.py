f94529ca8e7682d16572cfe5ffce0e715773ad0e
Richard Hadden <oculardexterity@gmail.com>
2015-03-10 14:32:19 +0000

    Extracted overwrite logic // Moved merge args to merge()

--- corpusbuilder/TransformCorpus.py
+++ corpusbuilder/TransformCorpus.py
@@ -5,43 +5,63 @@ import os
 
 class TransformCorpus():
 
-	def __init__(self, old_shelve_path, new_id, merge_column, new_shelve_path, break_string = "<pb/>"):
+	def __init__(self, old_shelve_path, new_shelve_path = "", test = False):
 		self.old_shelve_path = old_shelve_path
 		self.new_shelve_path = new_shelve_path
-		self.merge_column = merge_column
-		self.new_id = new_id
-		self.break_string = break_string
+		self.test = test
 	
 
 
-	def merge(self):
-		if os.path.isfile(self.new_shelve_path):
-				os.remove(self.new_shelve_path)
+	def merge(self, new_id, merge_column, break_string = "<pb/>"):
+		""" 
+		Merge function builds new corpus file with one merged field.
+		Inputs:
+			new_id: column in existing shelve to use as the new ID (e.g. Letter ID)
+			merge_column: column to be merged (e.g. the Letter text of multiple pages)
+			break_string: string for marking the join on merge (e.g. a <pb/> tag)
 
-	
+		"""
+		#
+		if self.new_shelve_path == "":
+			self.new_shelve_path = self.old_shelve_path.split(".")[0] + '_merge.shelve'
+
+
+		if self.prevent_overwrite(self.new_shelve_path) and self.test:
+			raise Exception('Merge halted to prevent overwrite')
+			return
+
 	
 		old_shelve = shelve.open(self.old_shelve_path)
 		new_shelve = shelve.open(self.new_shelve_path)
 
 		for key, row in sorted(old_shelve.items()):
 
-			current_row_key = str(row[self.new_id]).encode('ascii')
+			current_row_key = str(row[new_id]).encode('ascii')
 			#print 'CRK: ' +  current_row_key
 
 			if current_row_key in new_shelve:
 						 
-				row[self.merge_column] = new_shelve[current_row_key][self.merge_column] + self.break_string + row[self.merge_column]
+				row[merge_column] = new_shelve[current_row_key][merge_column] + break_string + row[merge_column]
 				new_shelve[current_row_key] = row
 			else:
 			
 				new_shelve[current_row_key] = row
 	
 
+	def prevent_overwrite(self, the_file):
+		if os.path.isfile(the_file):
+			if raw_input("Overwrite existing corpus? 'y' to confirm: ") == 'y':
+				os.remove(the_file)
+				return False
+			else:
+				return True
+		else:
+			return False
 			
 
 def main():
-	transform = TransformCorpus('corpus/corpus.shelve', 'Letter', 'Translation', 'corpus/letter_corpus.shelve')
-	transform.merge()
+	transform = TransformCorpus('corpus/corpus.shelve')
+	transform.merge('Letter', 'Translation')
 
 if __name__ == "__main__":
 	main()
--- test/test_fixcorpus.py
+++ test/test_fixcorpus.py
@@ -13,27 +13,27 @@ class TestMerge:
 
 	def setup(self):
 	
-		self.shelve_path = 'test/test_tmp/test.shelve'
+		self.shelve_path = 'test/test_tmp/'
 
-		self.extracted = Extractor('test/test_sheets/test_letter_merge.xlsx', self.shelve_path, 'PageID')
+		self.extracted = Extractor('test/test_sheets/test_letter_merge.xlsx', self.shelve_path + 'test.shelve', 'PageID')
 		self.extracted.buildShelveFile('DT')
 
-		self.new_shelve_path = 'test/test_tmp/new_test.shelve'
+		#self.new_shelve_path = 'test/test_tmp/new_test.shelve'
 
-		self.TransformCorpus = TransformCorpus(self.shelve_path, 'LetterID', 'merge_column', self.new_shelve_path)
+		self.TransformCorpus = TransformCorpus(self.shelve_path + 'test.shelve', test=True)
 
 		self.break_string = "<pb/>"
 	
 	def teardown(self):
-		if os.path.isfile(self.new_shelve_path):
-				os.remove(self.new_shelve_path)
-		if os.path.isfile(self.shelve_path):
-				os.remove(self.shelve_path)
+		if os.path.isfile(self.shelve_path + 'test_merge.shelve'):
+				os.remove(self.shelve_path + 'test_merge.shelve')
+		if os.path.isfile(self.shelve_path + 'test.shelve'):
+				os.remove(self.shelve_path + 'test.shelve')
 	
 
 	def test_merge(self):
-		self.TransformCorpus.merge()
-		new_corpus = Corpus(self.new_shelve_path)
+		self.TransformCorpus.merge('LetterID', 'merge_column')
+		new_corpus = Corpus(self.shelve_path + 'test_merge.shelve')
 		assert list(new_corpus.__iter__()) == [{u'DT': 41583.50037037037, u'PageID': u'page_ID2', u'A_HEADER': u'a_value3', u'merge_column': u'MergePart1<pb/>MergePart2', u'LetterID': u'letterID'}]
 
 	def test_strip_tags(self):
